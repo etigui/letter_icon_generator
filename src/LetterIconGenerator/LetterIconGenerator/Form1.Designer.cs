fa00df39e4b887ffaa1b40627164532bc2967308
etigui <32940684+etigui@users.noreply.github.com>
2018-12-12 23:51:16 +0100

    Save icon and image ok

--- src/LetterIconGenerator/LetterIconGenerator/Form1.Designer.cs
+++ src/LetterIconGenerator/LetterIconGenerator/Form1.Designer.cs
@@ -23,17 +23,25 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.BTSaveImage = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.CBSize = new System.Windows.Forms.ComboBox();
+            this.CBType = new System.Windows.Forms.ComboBox();
             this.PImage = new System.Windows.Forms.Panel();
+            this.CDColor = new System.Windows.Forms.ColorDialog();
+            this.TBLetter = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TBBackColor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TBLetterColor = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BTSaveImage
             // 
-            this.BTSaveImage.Location = new System.Drawing.Point(361, 404);
+            this.BTSaveImage.Location = new System.Drawing.Point(61, 165);
             this.BTSaveImage.Name = "BTSaveImage";
             this.BTSaveImage.Size = new System.Drawing.Size(75, 23);
             this.BTSaveImage.TabIndex = 14;
@@ -44,73 +52,133 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(293, 341);
+            this.label2.Location = new System.Drawing.Point(10, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(62, 13);
             this.label2.TabIndex = 13;
             this.label2.Text = "Image type:";
             // 
-            // label1
+            // CBType
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(325, 301);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 13);
-            this.label1.TabIndex = 12;
-            this.label1.Text = "Size:";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "16x16",
-            "32x32",
-            "64x64",
-            "256x256",
-            "512x512"});
-            this.comboBox1.Location = new System.Drawing.Point(361, 338);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 11;
-            // 
-            // CBSize
-            // 
-            this.CBSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.CBSize.FormattingEnabled = true;
-            this.CBSize.Items.AddRange(new object[] {
-            "16x16",
-            "32x32",
-            "64x64",
-            "256x256",
-            "512x512"});
-            this.CBSize.Location = new System.Drawing.Point(361, 298);
-            this.CBSize.Name = "CBSize";
-            this.CBSize.Size = new System.Drawing.Size(121, 21);
-            this.CBSize.TabIndex = 10;
+            this.CBType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CBType.FormattingEnabled = true;
+            this.CBType.Items.AddRange(new object[] {
+            "Icon",
+            "Png",
+            "Jpeg",
+            "Bmp",
+            "Gif"});
+            this.CBType.Location = new System.Drawing.Point(78, 41);
+            this.CBType.Name = "CBType";
+            this.CBType.Size = new System.Drawing.Size(121, 21);
+            this.CBType.TabIndex = 11;
             // 
             // PImage
             // 
-            this.PImage.Location = new System.Drawing.Point(272, 24);
+            this.PImage.Location = new System.Drawing.Point(0, 0);
+            this.PImage.Margin = new System.Windows.Forms.Padding(0);
             this.PImage.Name = "PImage";
             this.PImage.Size = new System.Drawing.Size(256, 256);
             this.PImage.TabIndex = 9;
+            // 
+            // TBLetter
+            // 
+            this.TBLetter.Location = new System.Drawing.Point(78, 68);
+            this.TBLetter.Name = "TBLetter";
+            this.TBLetter.Size = new System.Drawing.Size(121, 20);
+            this.TBLetter.TabIndex = 16;
+            this.TBLetter.Text = "L";
+            this.TBLetter.TextChanged += new System.EventHandler(this.TBLetter_TextChanged);
+            this.TBLetter.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TBLetter_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Letter:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Back color:";
+            // 
+            // TBBackColor
+            // 
+            this.TBBackColor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.TBBackColor.Location = new System.Drawing.Point(78, 98);
+            this.TBBackColor.Name = "TBBackColor";
+            this.TBBackColor.ReadOnly = true;
+            this.TBBackColor.Size = new System.Drawing.Size(121, 20);
+            this.TBBackColor.TabIndex = 18;
+            this.TBBackColor.Text = "Select color";
+            this.TBBackColor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TBBackColor.Click += new System.EventHandler(this.TBBackColor_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Letter color:";
+            // 
+            // TBLetterColor
+            // 
+            this.TBLetterColor.BackColor = System.Drawing.Color.White;
+            this.TBLetterColor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.TBLetterColor.Location = new System.Drawing.Point(78, 124);
+            this.TBLetterColor.Name = "TBLetterColor";
+            this.TBLetterColor.ReadOnly = true;
+            this.TBLetterColor.Size = new System.Drawing.Size(121, 20);
+            this.TBLetterColor.TabIndex = 20;
+            this.TBLetterColor.Text = "Select color";
+            this.TBLetterColor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TBLetterColor.Click += new System.EventHandler(this.TBLetterColor_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.CBType);
+            this.groupBox1.Controls.Add(this.TBLetterColor);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.TBBackColor);
+            this.groupBox1.Controls.Add(this.BTSaveImage);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.TBLetter);
+            this.groupBox1.Location = new System.Drawing.Point(262, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(209, 229);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.BTSaveImage);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.CBSize);
+            this.ClientSize = new System.Drawing.Size(476, 256);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.PImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Letter icon generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -118,10 +186,16 @@
 
         private System.Windows.Forms.Button BTSaveImage;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox CBSize;
+        private System.Windows.Forms.ComboBox CBType;
         private System.Windows.Forms.Panel PImage;
+        private System.Windows.Forms.ColorDialog CDColor;
+        private System.Windows.Forms.TextBox TBLetter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TBBackColor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TBLetterColor;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- src/LetterIconGenerator/LetterIconGenerator/Form1.cs
+++ src/LetterIconGenerator/LetterIconGenerator/Form1.cs
@@ -3,79 +3,180 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace LetterIconGenerator {
     public partial class Form1 : Form {
+
+        #region Init
+
         public Form1() {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e) {
+            CBType.SelectedIndex = 0;
+            TBBackColor.BackColor = Color.FromArgb(22, 160, 133);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e) {
+            DrawImage(false);
+        }
+
+        #endregion
+
+        #region Controls
+
         private void BTSaveImage_Click(object sender, EventArgs e) {
-            int size = 256;
-            String letter = "L";
-            Color myColor = System.Drawing.ColorTranslator.FromHtml("#9b59b6");
+            DrawImage(true);
+        }
 
-
-            //int rest_width = 256- sizeOfText.Width;
-            //int rest_height = 256 - sizeOfText.Height;
-
-            Size rectangleSize = new Size(PImage.Width, PImage.Height);
-
-            Size letterSize = TextRenderer.MeasureText(letter, new Font("Arial", 128, FontStyle.Bold));
-            int letterWidth = (PImage.Width / 2) - (letterSize.Width / 2) + PImage.Location.X;
-            int letterHeight = (PImage.Height / 2) - (letterSize.Height / 2) + PImage.Location.Y;
-
-
-            Graphics g = PImage.CreateGraphics();
-            Rectangle rect = new Rectangle(new Point(PImage.Location.X, PImage.Location.Y), rectangleSize);
-            g.FillRectangle(new SolidBrush(Color.FromArgb(myColor.A, myColor.R, myColor.G, myColor.B)), rect);
-            g.DrawString(letter, new Font("Arial", 128, FontStyle.Bold), Brushes.White, new Point(letterWidth, letterHeight));
-            //g.Dispose();
-
-
-            //Graphics gr1 = panel1.CreateGraphics();
-            //Bitmap bmp1 = new Bitmap(panel1.Width, panel1.Height);
-            //panel1.DrawToBitmap(bmp1, new Rectangle(0, 0, panel1.Width, panel1.Height));
-
-            string x = Application.StartupPath + @"\\xx.ico";
-            //bmp1.Save(x);
-
-            Image bmp = new Bitmap(PImage.Width, PImage.Height);
-            var gg = Graphics.FromImage(bmp);
-            var rects = PImage.RectangleToScreen(PImage.ClientRectangle);
-            gg.CopyFromScreen(rects.Location, Point.Empty, PImage.Size);
-
-
-            bmp.Save(x, ImageFormat.Icon);
-
-            //bmp.Save(x);
-            //Process.Start(x);
-
-
-            /*Bitmap newImage2 = new Bitmap(panel1.Width, panel1.Height, g);
-            newImage2.Save(x);
-            g.Dispose();*/
-
-            /*Graphics g = panel1.CreateGraphics();
-            Color myColor = System.Drawing.ColorTranslator.FromHtml("#9b59b6");
-            Pen p = new Pen(Color.FromArgb(myColor.A, myColor.R, myColor.G, myColor.B));
-            Rectangle r = new Rectangle(1, 1, 256, 256);
-            g.DrawRectangle(p, r);
-
-            using (Brush brush = new SolidBrush(Color.FromArgb(myColor.A, myColor.R, myColor.G, myColor.B)))    // specify color here and brush type here
-            {
-                g.FillRectangle(brush, r);
+        private void TBBackColor_Click(object sender, EventArgs e) {
+            CDColor.Color = TBBackColor.BackColor;
+            if (CDColor.ShowDialog() == DialogResult.OK) {
+                TBBackColor.BackColor = CDColor.Color;
+                DrawImage(false);
             }
-            label1.BackColor = Color.Transparent;
-            label1.BringToFront();*/
+        }
 
+        private void TBLetterColor_Click(object sender, EventArgs e) {
+            CDColor.Color = TBLetterColor.BackColor;
+            if (CDColor.ShowDialog() == DialogResult.OK) {
+                TBLetterColor.BackColor = CDColor.Color;
+                DrawImage(false);
+            }
+        }
 
+        /// <summary>
+        /// Check letter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TBLetter_KeyPress(object sender, KeyPressEventArgs e) {
+            
+            // Check if not backspace
+            if (e.KeyChar == '\u0008') {
+            } else {
 
-            //panel1.Refresh();
+                e.KeyChar = char.Parse((e.KeyChar.ToString().ToUpper()));
+
+                // Check letter and number
+                if (!char.IsLetter(e.KeyChar)) {
+                    e.Handled = true;
+                }
+
+                // Only one char is allowed
+                if (TBLetter.Text.Count() > 0) {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void CBSize_SelectedIndexChanged(object sender, EventArgs e) {
+            DrawImage(false);
+        }
+
+        private void TBLetter_TextChanged(object sender, EventArgs e) {
+            DrawImage(false);
+        }
+
+        #endregion
+
+        #region Image
+
+        /// <summary>
+        /// Create icon image
+        /// </summary>
+        /// <param name="filePathTmp"></param>
+        /// <param name="letter"></param>
+        private void SaveIcon(string filePathTmp, string letter) {
+
+            // Create icon with png image
+            using (FileStream stream = File.OpenWrite(Path.Combine(Application.StartupPath, $"{letter}_{DateTime.Now.Ticks}.ico"))) {
+                Bitmap bitmap = (Bitmap)Image.FromFile(filePathTmp);
+                Icon.FromHandle(bitmap.GetHicon()).Save(stream);
+                bitmap.Dispose();
+            }
+
+            // Delete png image temp
+            try {
+                if (File.Exists(filePathTmp)) {
+                    File.Delete(filePathTmp);
+                }
+            } catch { }
+        }
+
+        /// <summary>
+        /// Create image
+        /// </summary>
+        /// <param name="letter"></param>
+        /// <param name="icon"></param>
+        /// <returns></returns>
+        private string SaveImage(string letter, bool icon) {
+            // Save panel as image
+            string filePath = String.Empty;
+            Image bmp = new Bitmap(PImage.Width, PImage.Height);
+            Graphics gImage = Graphics.FromImage(bmp);
+            Rectangle rects = PImage.RectangleToScreen(PImage.ClientRectangle);
+            gImage.CopyFromScreen(rects.Location, Point.Empty, PImage.Size);
+            if (icon) {
+                filePath = Path.Combine(Application.StartupPath, $"{letter}_{DateTime.Now.Ticks}.png");
+                bmp.Save(filePath, ImageFormat.Png);
+            } else {
+                filePath = Path.Combine(Application.StartupPath, $"{letter}_{DateTime.Now.Ticks}.{CBType.Text.ToLower()}");
+                bmp.Save(filePath, ParseImageFormat(CBType.Text));
+            }
+            return filePath;
+        }
+
+        /// <summary>
+        /// Draw image on panel
+        /// </summary>
+        /// <param name="save"></param>
+        private void DrawImage(bool save) {
+
+            // Check if letter
+            string letter = TBLetter.Text;
+            if (letter != "") {
+
+                // Image info
+                int size = PImage.Width;
+                int location = PImage.Location.X;
+                Color letterColor = TBLetterColor.BackColor;
+                Color backColor = TBBackColor.BackColor;
+                Font font = new Font("Arial", size / 2, FontStyle.Bold);
+                Size letterSize = TextRenderer.MeasureText(letter, font);
+                int letterWidth = (size / 2) - (letterSize.Width / 2);
+                int letterHeight = (size / 2) - (letterSize.Height / 2);
+
+                // Create image on panel (backgroud and letter)
+                Graphics gPanel = PImage.CreateGraphics();
+                Rectangle rect = new Rectangle(new Point(location, location), new Size(size, size));
+                gPanel.FillRectangle(new SolidBrush(Color.FromArgb(backColor.A, backColor.R, backColor.G, backColor.B)), rect);
+                gPanel.DrawString(letter, font, new SolidBrush(letterColor), new Point(letterWidth + 10, letterHeight));
+                if (save) {
+
+                    // Save as icon
+                    if (CBType.Text.EndsWith("n")) {
+                        SaveIcon(SaveImage(letter, true), letter);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Convert string extension to image format
+        /// </summary>
+        /// <param name="imageFormat"></param>
+        /// <returns></returns>
+        public static ImageFormat ParseImageFormat(string imageFormat) {
+            return (ImageFormat)typeof(ImageFormat).GetProperty(imageFormat, BindingFlags.Public | BindingFlags.Static | BindingFlags.IgnoreCase).GetValue(null);
         }
     }
 }
